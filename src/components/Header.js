f389884cb6d6cfbd23700eab520db58d87be9af1
Ahmed Hakeem <ahmedhakeem@Ahmeds-MacBook-Pro.local>
2021-12-12 13:16:43 -0500

    finsh from the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,41 @@
-import React from 'react'
-import './Header.css'
-import punkLogo from '../assets/header/cryptopunk-logo.png'
-import searchIcon from '../assets/header/search.png'
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitcIcon from "../assets/header/theme-switch.png"
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className='logoContainer'>
-                <img src={punkLogo} className="punkLogo"  alt='logo'/>
-                <div className='searchBar'>
-                    <div className='searchIconContainer'>
-                        <img src={searchIcon} alt='searchIcon' />
-                    </div>
-                    <input type='text' placeholder='Search' className='searchInput'/>
-                </div>
-            </div>
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="logo" />
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="searchIcon" />
         </div>
-    )
-}
+        <input
+          type="text"
+          placeholder="Collection, item or user ..."
+          className="searchInput"
+        />
+      </div>
 
-export default Header
+      <div className='headerItems'>
+          <p>Drops</p>
+          <p>Marketplace</p>
+          <p>Create</p>
+      </div>
+
+      <div className='headerAction'>
+          <div className='themeSwitchContainer'>
+              <img src={themeSwitcIcon} alt="themeSwitcIcon" />
+          </div>
+      </div>
+      <div className='loginButton'>GET IN</div>
+
+    </div>
+  );
+};
+
+export default Header;

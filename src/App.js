9a436f777ea0117a578ac5c181504d234276d6bc
Ahmed Hakeem <ahmedhakeem@Ahmeds-MacBook-Pro.local>
2021-12-12 18:25:13 -0500

    finsh from the card list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
+import axios from 'axios';
+import Punklist from "./components/Punklist";
 
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+     const getMyNfts = async () => {
+       const openseaData = await axios.get('https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0xFD48646F157B732c0cbD8730a76e40442442e2eB&order_direction=asc')
+       console.log(openseaData.data.assets)
+        setPunkListData(openseaData.data.assets)
+     }
+     return getMyNfts()
+  }, [])
+
   return (
     <div className="app">
       <Header />
+      
+      <Punklist punkListData={punkListData}/>
+
     </div>
   );
 }
---
+++ src/components/Punklist.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import CollectionCard from './CollectionCard'
+import './Punklist.css'
+
+const Punklist = ({punkListData}) => {
+    return (
+        <div className='punklist'>
+            {punkListData.map(punk => (
+                <div>
+                    <CollectionCard
+                    key={punk.token_id}
+                    id={punk.token_id}
+                    name={punk.name}
+                    traits={punk.traits}
+                    image={punk.image_original_url}
+                    />
+                </div>
+
+            ))}
+        </div>
+    )
+}
+
+export default Punklist
